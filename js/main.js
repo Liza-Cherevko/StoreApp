c5d220aecf443f17cfba44fbae9c18c264e4ceba
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-11-28 15:22:04 +0200

    last commit

--- js/main.js
+++ js/main.js
@@ -49,11 +49,11 @@ document.querySelector('button.add_new').addEventListener('click', (e)=> {
 })
 // Обновление отображаемых товаров
 updateGoods()
-function updateGoods() { 
-    const resultPrice = 0;
+function updateGoods() {
+    let resultPrice = 0;
     const tBody = document.querySelector('.list');
     tBody.innerHTML = '';
-    document.querySelector('.card').innerHTML = '';
+    document.querySelector('.cards').innerHTML = '';
     let goods = JSON.parse(localStorage.getItem('goods'))
     if (goods.length) {
         table1.hidden = false;
@@ -61,36 +61,36 @@ function updateGoods() {
         for (let i = 0; i < goods.length; i++) {
             tBody.insertAdjacentHTML('beforeend',
                 `
-                   <tr class="align-middle">
-                   <td>${i + 1}</td>
-                   <td class="name">${goods[i][1]}</td>
-                   <td class="price">${goods[i][2]}</td>
-                   <td >${goods[i][3]}</td>
-                   <td ><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
-                   <td ><button class="good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
-                   </tr>
+                <tr class="align-middle">
+                <td>${i+1}</td>
+                <td class="name">${goods[i][1]}</td>
+                <td class="price">${goods[i][2]}</td>
+                <td>${goods[i][3]}</td>
+                <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
+                <td><button class="good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
+              </tr>
                 `)
             if (goods[i][4] > 0) {
                 // Расчитать цену с учетом скидки
                 goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4] * goods[i][2] * goods[i][5] * 0.01
                 // Сумировать цену в общий результат
-                result_price += goods[i][6]
-                document.querySelector('.card').insertAdjacentHTML('beforeend',
-                    `
+                resultPrice += goods[i][6]
+                document.querySelector('.cards').insertAdjacentHTML('beforeend',
+                `
                 <tr class="align-middle">
-                <td>${i + 1}</td>
-                <td class="price_name">${goods[i][1]}</td>
-                <td class="price_one">${goods[i][2]}</td>
-                <td class="price_count">${goods[i][4]}</td>
-                <td class="price_discount"><input data-goodid="${goods[i][0]}" type="text" value="${goods[i][5]}" min="0" max="100"></td>
-                <td> ${goods[i][6]}</td>
-                <td ><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
+                  <td>${i+1}</td>
+                  <td class="price_name">${goods[i][1]}</td>
+                  <td class="price_one">${goods[i][2]}</td>
+                  <td class="price_count">${goods[i][4]}</td>
+                  <td class="price_discount"><input data-goodid="${goods[i][0]}" type="text" value="${goods[i][5]}" min="0" max="100"></td>
+                  <td>${goods[i][6]}</td>
+                  <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
                 </tr>
                 `)
             }
         }
             userList = new List('goods', options);
-    } else { 
+    } else {
         table1.hidden = true
         table2.hidden = true
     }
@@ -99,10 +99,10 @@ function updateGoods() {
 
 // удаление товаров
 
-document.querySelector('.list').addEventListener('click', (e) => { 
+document.querySelector('.list').addEventListener('click', (e) => {
     if (!e.target.dataset.delete) {
         return
-    } 
+    }
     Swal.fire({
         title: 'Attention',
         text: 'Are you sure you want to delete the item?',
@@ -112,11 +112,11 @@ document.querySelector('.list').addEventListener('click', (e) => {
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!',
         cancelButtonText: 'No'
-    }).then((result) => { 
+    }).then((result) => {
         if (result.isConfirmed) {
             let goods = JSON.parse(localStorage.getItem('goods'))
-            for (let i = 0; i < goods.length; i++) { 
-                if (goods[i][0] == e.target.dataset.delete) { 
+            for (let i = 0; i < goods.length; i++) {
+                if (goods[i][0] == e.target.dataset.delete) {
                     goods.splice(i, 1)
                     localStorage.setItem('goods', JSON.stringify(goods))
                     updateGoods()
@@ -125,6 +125,96 @@ document.querySelector('.list').addEventListener('click', (e) => {
             Swal.fire(
                 'The product has been removed'
             )
-  } 
+  }
 })
 })
+// добавление товара
+document.querySelector('.list').addEventListener('click', (e) => {
+    if (!e.target.dataset.goods) {
+        return
+    }
+    let goods = JSON.parse(localStorage.getItem('goods'))
+    for (let i = 0; i < goods.length; i++) {
+        if (goods[i][3] >0 &&goods[i][0]==e.target.dataset.goods) {
+            goods[i].splice(3, 1, goods[i][3] - 1)
+            goods[i].splice(4, 1, goods[i][4] + 1)
+            localStorage.setItem('goods', JSON.stringify(goods))
+            updateGoods()
+        }
+       
+    }
+})
+// удаление товара 
+document.querySelector('.cards').addEventListener('click', (e) => {
+    if (!e.target.dataset.delete) {
+        return
+    }
+    let goods = JSON.parse(localStorage.getItem('goods'))
+    for (let i = 0; i < goods.length; i++) {
+        if (goods[i][4] >0 &&goods[i][0]==e.target.dataset.delete) {
+            goods[i].splice(3, 1, goods[i][3] + 1)
+            goods[i].splice(4, 1, goods[i][4] - 1)
+            localStorage.setItem('goods', JSON.stringify(goods))
+            updateGoods()
+        }
+       
+    }
+})
+
+
+// сортировка таблиц 
+function sortTable(colNum, type, id) { 
+    let elem = document.getElementById(id)
+    let tBody = elem.querySelector('tbody')
+    let rowsArray = Array.from(tBody.rows)
+    let compare
+
+    switch (type) {
+        case 'number':
+            compare = function (rowA, rowB) { 
+                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
+            }
+            break;
+            case 'string':
+                compare = function (rowA, rowB) { 
+                    return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1:-1
+                }
+                break;
+    }
+    rowsArray.sort(compare)
+    tBody.append(...rowsArray)
+}
+
+table1.onclick = function (e) { 
+    if (e.target.tagName != 'TH') return
+    let th = e.target
+    sortTable(th.cellIndex, th.dataset.type, 'table1')
+}
+
+table2.onclick = function (e) { 
+    if (e.target.tagName != 'TH') return
+    let th = e.target
+    sortTable(th.cellIndex, th.dataset.type, 'table2')
+}
+
+// скидка 
+document.querySelector('.cards').addEventListener('input', function(e) {  
+    if(!e.target.dataset.goodid) {
+      return
+    }
+    let goods = JSON.parse(localStorage.getItem('goods'))
+    for(let i=0; i<goods.length; i++) {
+      if(goods[i][0] == e.target.dataset.goodid) {
+        // Скидка
+        goods[i][5] = e.target.value
+        // Цена со скидкой
+        goods[i][6] = goods[i][4]*goods[i][2] - goods[i][4]*goods[i][2]*goods[i][5]*0.01
+        localStorage.setItem('goods', JSON.stringify(goods))
+        updateGoods()
+        // Поставить фокус в поле скидки и передвинуть курсор в конец
+        let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`)
+        input.focus()
+        input.selectionStart = input.value.length;
+      }
+    }
+  })
